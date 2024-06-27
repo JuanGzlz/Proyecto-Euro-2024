499e97fa6185baa9387a7fd7dbe47aae6a285b3f
Juan González <155971097+JuanGzlz@users.noreply.github.com>
2024-06-27 02:42:59 -0400

    6th commit

--- App.py
+++ App.py
@@ -86,21 +86,17 @@ Bienvenido/a a la búsqueda de partidos de la Eurocopa 2024
                 opcion = input("Ingrese el número de la opción que desea elegir: ")
 
             if opcion == "1":
-                i = 0
-                for partido in self.partidos:
-                    i += 1
-                    print(f"----------- {i} -----------")
+                for i, partido in enumerate(self.partidos):
+                    print(f"------------- {i+1} -------------")
                     print(partido.show())
             
             elif opcion == "2":
                 eleccion_pais = input("Ingrese el nombre o código FIFA de un país (en inglés): ").lower()
                 contador = 0
-                i = 0
-                for partido in self.partidos:
+                for i, partido in enumerate(self.partidos):
                     if eleccion_pais in partido.local.nombre.lower() or eleccion_pais in partido.visitante.nombre.lower():
                         contador = 1
-                        i += 1
-                        print(f"----------- {i} -----------")
+                        print(f"------------- {i+1} -------------")
                         print(partido.show())
                 if contador == 0:
                     print("El país ingresado no fue conseguido")
@@ -108,12 +104,10 @@ Bienvenido/a a la búsqueda de partidos de la Eurocopa 2024
             elif opcion == "3":
                 eleccion_estadio = input("Ingrese un estadio de la Eurocopa: ").lower()
                 contador = 0
-                i = 0
-                for partido in self.partidos:
+                for i, partido in enumerate(self.partidos):
                     if eleccion_estadio in partido.id_estadio.nombre.lower():
                         contador = 1
-                        i += 1
-                        print(f"----------- {i} -----------")
+                        print(f"------------- {i+1} -------------")
                         print(partido.show())
                 if contador == 0:
                     print("El estadio ingresado no fue conseguido")
@@ -121,12 +115,10 @@ Bienvenido/a a la búsqueda de partidos de la Eurocopa 2024
             elif opcion == "4":
                 eleccion_fecha = input("Ingrese la fecha que desea ver un partido (AAAA-MM-DD): ")
                 contador = 0
-                i = 0
-                for partido in self.partidos:
+                for i, partido in enumerate(self.partidos):
                     if eleccion_fecha in partido.fecha:
                         contador = 1
-                        i += 1
-                        print(f"----------- {i} -----------")
+                        print(f"------------- {i+1} -------------")
                         print(partido.show())
                 if contador == 0:
                     print("No hay partidos en la fecha ingresada")
@@ -139,33 +131,115 @@ Bienvenido/a a la búsqueda de partidos de la Eurocopa 2024
 Bienvenido/a al registro OFICIAL de su persona en el sistema de la Eurocopa 2024...
 """)
         
-        nombre = input("Ingrese su primer nombre: ").capitalize()
+        nombre = input("Ingrese su primer nombre: ").capitalize().strip()
         while nombre == "" or not nombre.isalpha():
             print("Ingreso inválido...")
-            nombre = input("Ingrese su nombre completo: ")
+            nombre = input("Ingrese su primer nombre: ").capitalize().strip()
 
-        apellido = input("Ingrese su primer apellido: ").capitalize()
+        apellido = input("Ingrese su primer apellido: ").capitalize().strip()
         while apellido == "" or not apellido.isalpha():
             print("Ingreso inválido...")
-            apellido = input("Ingrese su nombre completo: ")
+            apellido = input("Ingrese su primer apellido: ").capitalize().strip()
 
         nombre_completo = nombre + " " + apellido
 
-        dni = input("Ingrese su cédula/DNI: ")
+        dni = input("Ingrese su cédula/DNI: ").strip()
         while len(dni) == 0 or not dni.isnumeric():
             print("Ingreso inválido...")
-            dni = input("Ingrese su cédula/DNI: ")
+            dni = input("Ingrese su cédula/DNI: ").strip()
 
-        edad = input("Ingrese sus años de edad: ")
-        while edad <= 0 or edad > 100 or not edad.isnumeric():
-            print("Ingreso inválido...")
-            edad = input("Ingrese sus años de edad: ")
+        while True:
+            try:
+                edad = int(input("Ingrese sus años de edad: "))
+                if edad > 0 and edad < 100:
+                    break
+                else:
+                    print("Ingreso inválido, coloque una edad real...")
+            except ValueError:
+                print("Ingreso inválido...")
 
-        print("¡El cliente ha sido registrado exitosamente!")
+        print("""
+¡El cliente ha sido registrado exitosamente!
+""")
         cliente = Cliente(nombre_completo, dni, edad)
         cliente.descuentos()
     
         return cliente
+
+    def seleccion_partido(self, partidos_disponibles):
+        print("""
+PARTIDOS DISPONIBLES
+""")
+        for i, partido in enumerate(partidos_disponibles):
+            print(f"------------- {i+1} -------------")
+            print(partido.show())
+            print(f"/////////////////////////////////")
+
+        opcion = input("Ingrese el número de la opción que desea elegir: ")
+        while not opcion.isnumeric() or int(opcion) not in range(1, len(partidos_disponibles)):
+            print("Ingreso inválido...")
+            opcion = input("Ingrese el número de la opción que desea elegir: ")
+        
+        print(f"""
+PARTIDO SELECCIONADO: {partidos_disponibles[opcion - 1].local.nombre} VS. {partidos_disponibles[opcion - 1].visitante.nombre}
+""")
+        return partidos_disponibles[opcion - 1]
+
+    def mapa_disponibilidad(self, juego_selec):
+        mapa_estadio = []
+        columnas = 10
+        limite = juego_selec.estadio.capacidad[0] + juego_selec.estadio.capacidad[1]
+        filas = limite // columnas
+        lista_abc = []
+        abc = "ABCDEFGHIJ"
+        for letra1 in abc:
+            for letra2 in abc:
+                lista_abc.append(letra1 + letra2)
+
+        asientos = []
+        for i in range(filas):
+            for j in range(1, columnas + 1):
+                asientos.append(f"{lista_abc[i]}{j}")
+
+        asientos_disponibles = []
+        for asiento in asientos:
+            if asiento not in juego_selec.asientos_tomados:
+                asientos_disponibles.append(asiento)
+
+        for i in lista_abc[:filas]:
+            fila = []
+            for j in range(1, columnas + 1):
+                asiento1 = f"{i}{j}"
+                if asiento1 in juego_selec.asientos_tomados:
+                    fila.append(f"|   X  ")
+                else:
+                    fila.append(f"|  {asiento1} ")
+            fila.append("|")
+            mapa_estadio.append("".join(fila))
+        
+        seccion_actual = None
+        for fila1 in mapa_estadio:
+            seccion = fila1.split("|")[1].strip()[0]
+            if seccion != seccion_actual:
+                seccion_actual = seccion
+                if seccion_actual != "A":
+                    print("-" * len(fila1))
+                print(f"""
+
+============================== ZONA: '{seccion_actual}' ===============================
+""")
+            print("-" * len(fila1))
+            print(fila1)
+        print("-" * len(mapa_estadio[-1]))
+
+        asiento_elegido = input("Ingrese el asiento desde donde desee ver el partido (Ej: AA1): ")
+        while asiento_elegido not in asientos_disponibles:
+            print("El asiento ingresado NO existe o está OCUPADO...")
+
+        print(f"""
+ASIENTO SELECCIONADO: {asiento_elegido}
+""")
+        return asiento_elegido
 
     def compra_entrada(self):
         ans = input("""
@@ -181,7 +255,7 @@ Bienvenido/a al registro OFICIAL de su persona en el sistema de la Eurocopa 2024
                 dni = input("Ingrese su cédula/DNI: ")
             
             if self.binary_search(self.clientes, 0, len(self.clientes) - 1, dni, lambda x: x.dni) == -1:
-                print("¡El cliente no se encuentra registrado! Su DNI no se encontró.")
+                print("¡El cliente no se encuentra registrado! Su DNI no se encontró...")
                 self.compra_entrada()
             
             ind = self.binary_search(self.clientes, 0, len(self.clientes) - 1, dni, lambda x: x.dni)
@@ -190,10 +264,22 @@ Bienvenido/a al registro OFICIAL de su persona en el sistema de la Eurocopa 2024
         else:
             cliente = self.registro_cliente()
 
-        if cliente.descuento_entrada():
-            print("¡Por su DNI, ha sido beneficiado con un 50% de descuento en la compra de entradas!")
-
+        partidos_disponibles = []
+        for partido in self.partidos:
+            if partido.entradas_general > 0 or partido.entradas_vip > 0:
+                partidos_disponibles.append(partido)
         
+        juego_selec = self.seleccion_partido(partidos_disponibles)
+
+
+        # if cliente.descuento_entrada():
+        #     print("¡Por su DNI, ha sido beneficiado con un 50% de descuento en la compra de entradas!")
+
+        print("""
+ENTRADAS DISPONIBLES
+--------------------
+""")
+
 
     def binary_search(self, list, min, max, x, key = lambda x: x):
         if len(list) != 0:
